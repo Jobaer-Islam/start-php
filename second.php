7eea7c5771b8c0895701469337571991364a89f7
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-04-25 20:56:37 +0600

    input field

---
+++ second.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Second page</h1>
+
+    <?php
+    $userName = $_GET['user_name'];
+    echo $userName;
+    $userID = $_GET['user_ID'];
+    echo $userID;
+    ?>
+
+</body>
+</html>
+
